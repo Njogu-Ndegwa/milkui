41beddc6cf6dfd69e221df77abb4fc522e6670fb
Njogu-Ndegwa <dennisndegwa001@gmail.com>
2020-10-14 04:12:59 +0300

    Mock Login

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,71 +1,74 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {routes }from '../pages'
+import { Switch, Route } from 'react-router-dom';
+import { routes } from '../pages'
 import 'antd/dist/antd.css';
 import '../index.css';
 import data from './data.json';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
-import Result from './404'
+import Result from './404';
+import { ProtectedRoute } from './protectedroute';
+import { Login } from '../pages/login';
 // import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
 // const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
-export default function View () {
-    return (
-  <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-       {data.map(item => (
-                  <Menu.Item key={item.key}>
-                      <Link to={item.url} >{item.title}</Link>
-                  </Menu.Item>
-              ))}
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-        <Sider className="site-layout-background" width={200}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%' }}
-          >
+export default function View() {
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          {data.map(item => (
+            <Menu.Item key={item.key}>
+              <Link to={item.url} >{item.title}</Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+            >
               {data.map(item => (
-                  <Menu.Item key={item.key}>
-                      <Link to={item.url} >{item.title}</Link>
-                  </Menu.Item>
+                <Menu.Item key={item.key}>
+                  <Link to={item.url} >{item.title}</Link>
+                </Menu.Item>
               ))}
-           
- 
-          </Menu>
-        </Sider>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
-          <Switch>
-      {
-        routes.map(route=> (
-          <Route exact={route.exact} path={route.path}>
-            {route.component}
-          </Route>
 
-        ))
-      }
 
-      <Route component={Result} />
-      </Switch>
-      
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Switch>
+              {
+                routes.map(route => (
+                  <ProtectedRoute
+                    exact={route.exact}
+                    path={route.path}
+                    component={route.component} />
+
+                ))
+              }
+              <Route path='/login' exact component={Login} />
+              <Route component={Result} />
+            </Switch>
+
+          </Content>
+        </Layout>
       </Content>
-      </Layout>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-  </Layout>
-    )
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  )
 }
---
+++ src/pages/login.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+import {AuthService} from '../components/auth_service'
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+export const Login = (props) => {
+const [redirect, setRedirect] = useState(false)
+
+const loginHandler = () => {
+    AuthService.login();
+    setRedirect(true)
+}
+
+let {from} = props.location.state || {from: {pathname: '/option1'}};
+if(redirect) return <Redirect to ={ from} />
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    loginHandler();
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+console.log(redirect, "redirect")
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
